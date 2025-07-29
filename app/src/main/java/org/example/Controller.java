38bc84dadecb069c16ad98bb971d817a85c90af2
Relja <relja.pesic@gmail.com>
2025-07-29 12:35:40 +0200

    got the song to play yay!

--- app/src/main/java/org/example/Controller.java
+++ app/src/main/java/org/example/Controller.java
@@ -1,12 +1,33 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import javafx.fxml.FXML;
 
 public class Controller {
 
   @FXML
   public void onPlayButton() {
-    System.out.println("Play Button clicked!");
+    String path = getClass().getResource("/song1.wav").getPath();
+    System.out.println("Play Button clicked! Path: " + path);
+    File file = new File(path);
+    try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file)) {
+      Clip clip = AudioSystem.getClip();
+      clip.open(audioInputStream);
+      clip.start();
+    } catch (UnsupportedAudioFileException e) {
+      System.err.println("Unsupported audio file: " + e.getMessage());
+    } catch (IOException e) {
+      System.err.println("IO error while accessing the file: " + e.getMessage());
+    } catch (Exception e) {
+      System.err.println("Some other error playing file: " + e.getMessage());
+    }
   }
 
   @FXML
---
+++ app/src/main/java/org/example/Playlist.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.List;
+
+public class Playlist {
+  private String name;
+  private List<Song> songs;
+  private int songCount;
+
+  public Playlist(List<Song> songs, int songCount, String name) {
+    this.songs = songs;
+    this.songCount = songCount;
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void addSong(Song song) {
+    songs.add(song);
+    songCount++;
+    System.out.println("Added song: " + song.getTitle() + " to playlist: " + name);
+  }
+
+  public void displaySongs() {
+    System.out.println("Playlist: " + name);
+    for (int i = 0; i < songCount; i++) {
+      System.out.println(songs.get(i).toString());
+    }
+  }
+}
